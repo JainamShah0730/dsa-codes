dfd45abea6513b5a9c221e34a81d88bebe94af18
JainamShah0730 <143824167+JainamShah0730@users.noreply.github.com>
2026-01-08 02:38:29 +0530

    Add files via upload

---
+++ removeelement.java
@@ -0,0 +1,14 @@
+package jainamproject;
+
+class removeelement {
+    public int removeElement(int[] nums, int val) {
+        int j = 0 ;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(val!= nums[i]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
